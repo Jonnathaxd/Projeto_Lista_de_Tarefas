d1f0ee6d4fea2f7ceaa47904666498e41dba5be5
Geova Jonnatha <geova_jonnatha@hotmail.com>
2021-01-01 19:25:40 -0300

    atualização

--- js/lista.js
+++ js/lista.js
@@ -5,7 +5,7 @@ onload = function(){
         let itens = localStorage.getItem('tarefas')
         itens = JSON.parse(itens)
         if (itens != null){
-            for(var item of itens.tare){
+            for(var item of itens.tare){    
                 lista.innerHTML += `<li>${item}</li>`
             }
         }
@@ -19,19 +19,24 @@ function mostraButton() {
 
 function adicionaItem(){
     let tarefa = document.getElementById('nova_tarefa').value
-    if(localStorage.getItem('tarefas') == null){
-        lista_tare.tare.push(tarefa)
-        localStorage.setItem('tarefas', JSON.stringify(lista_tare))
-        //console.log(lista_tare)
+    if(tarefa != null && tarefa != ''){
+        if(localStorage.getItem('tarefas') == null){
+            lista_tare.tare.push(tarefa)
+            localStorage.setItem('tarefas', JSON.stringify(lista_tare))
+            //console.log(lista_tare)
+        }
+        else{
+            let lista_tare = JSON.parse(localStorage.getItem('tarefas'))
+            lista_tare.tare.push(tarefa)
+            localStorage.setItem('tarefas', JSON.stringify(lista_tare))
+            //console.log(lista_tare)
+        }
+        let lista = document.getElementById('tarefas')
+        lista.innerHTML += `<li>${tarefa}</li>`
     }
-    else{
-        let lista_tare = JSON.parse(localStorage.getItem('tarefas'))
-        lista_tare.tare.push(tarefa)
-        localStorage.setItem('tarefas', JSON.stringify(lista_tare))
-        //console.log(lista_tare)
+    else {
+        alert('ERRO! VALOR DO CAMPO NÃO PODE SER VAZIO, VERIFIQUE O VALOR DIGITADO :(')
     }
-    let lista = document.getElementById('tarefas')
-    lista.innerHTML += `<li>${tarefa}</li>`
 }
 
 function apaga(){
@@ -48,13 +53,36 @@ function editar(){
         document.getElementById('botoes').style.display = 'none'
         let lista = document.getElementById('tarefas')
         lista.innerHTML = ''
-        dados.tare.forEach((element, i) => {
-            lista.innerHTML += `<input type="checkbox" name='valor' clicado='off' onClick="teste_box(this)" value='${i}'><li>${element}</li>`
+        dados.tare.forEach((element) => {
+            lista.innerHTML += `<input type="checkbox" name='valor' clicado='off' onClick="teste_box(this)" value='${element}'><li>${element}</li>`
         });
 
     }
     else {
         alert('ERRO! Não há nenhuma tarefa para editar.. :(')
+    }
+}
+function apaga_parte(){
+    let valor = document.getElementById('tarefas').children
+    let novas_tarefas = []
+    let tem_selecao = false
+    for(var item of valor){
+        if (item.getAttribute('clicado') == 'on'){
+            tem_selecao = true
+        }
+    }
+    if(tem_selecao){
+        for(var element of valor){
+            if(element.getAttribute('clicado') == 'off'){
+                novas_tarefas.push(`${element.getAttribute('value')}`)
+                }
+            }
+        lista_tare = {'tare': novas_tarefas} 
+        localStorage.setItem('tarefas', JSON.stringify(lista_tare))
+        voltar()
+    }
+    else{
+        alert('ERRO! VOCÊ NAO SELECIOU NADA PARA APAGAR :(')
     }
 }
 
